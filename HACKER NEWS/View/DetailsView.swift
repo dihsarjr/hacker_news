aeb1f8cd611ff7f56cdeb80c9133ff8745cec968
rashid <mohammed1rashid786@gmail.com>
2021-06-09 21:39:33 +0530

    finished

--- HACKER NEWS/View/DetailsView.swift
+++ HACKER NEWS/View/DetailsView.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
+
 struct DetailsView: View {
     let url: String?
     
     
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       WebView(urlString: url)
     }
 }
 
@@ -22,3 +23,6 @@ struct DetailsView_Previews: PreviewProvider {
         DetailsView(url: "https://www.google.com")
     }
 }
+
+
+
